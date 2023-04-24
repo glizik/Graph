0da860cfc12c9e16503b14e646f732ac16607c33
Gabor Lizik <glizik@gmail.com>
2023-04-24 15:40:02 +0200

    refactor VertexView

--- GraphSwift/GraphSwift/View/VertexView.swift
+++ GraphSwift/GraphSwift/View/VertexView.swift
@@ -8,32 +8,59 @@
 import SwiftUI
 
 struct VertexView: View {
-    static let width = CGFloat(100)
-    
     @State var vertex: Vertex
     
     @ObservedObject var selection: SelectionHandler
-    
-    var isSelected: Bool {
-        return selection.isVertexSelected(vertex)
-    }
     
     var body: some View {
         Ellipse()
           .fill(Color.green)
           .overlay(Ellipse()
-            .stroke(isSelected ? Color.red : Color.black, lineWidth: isSelected ? 5 : 3))
+            .stroke(strokeColor(),
+                lineWidth: lineWidth()))
           .overlay(Text(String(describing: vertex))
             .multilineTextAlignment(.center)
-            .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8)))
-          .frame(width: VertexView.width, height: VertexView.width, alignment: .center)
+            .padding(EdgeInsets(top: 0,
+                                leading: DrawingConstants.leadingPadding,
+                                bottom: 0,
+                                trailing: DrawingConstants.trailingPadding)))
+          .frame(width: DrawingConstants.width, height: DrawingConstants.width, alignment: .center)
+    }
+    
+    // MARK: -- private
+    
+    private var isSelected: Bool {
+        return selection.isVertexSelected(vertex)
+    }
+    
+    private func strokeColor() -> Color {
+        isSelected ? DrawingConstants.selectedColor :
+            DrawingConstants.unSelectedColor
+    }
+    
+    private func lineWidth() -> CGFloat {
+        isSelected ? DrawingConstants.selectedWidth :
+            DrawingConstants.unSelectedWidth
+    }
+    
+    private struct DrawingConstants {
+        static let width = CGFloat(300)
+        
+        static let selectedColor: Color = .red
+        static let unSelectedColor: Color = .black
+        
+        static let selectedWidth: CGFloat = 5
+        static let unSelectedWidth: CGFloat = 3
+        
+        static let leadingPadding: CGFloat = 8
+        static let trailingPadding: CGFloat = 8
     }
 }
 
 struct VertexView_Previews: PreviewProvider {
     static var previews: some View {
         let selection1 = SelectionHandler()
-        let vertex1 = Vertex(text: "Hello world")
+        let vertex1 = Vertex(text: "Hello world, Hello world, Hello world, Hello world")
         let selection2 = SelectionHandler()
         let vertex2 = Vertex(text: "I'm selected")
         selection2.selectVertex(vertex2)
@@ -44,3 +71,5 @@ struct VertexView_Previews: PreviewProvider {
         }
      }
 }
+
+
