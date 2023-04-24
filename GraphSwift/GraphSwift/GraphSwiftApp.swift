d5a130012daaeee219c3d9c4f110a05150c83eff
Gabor Lizik <glizik@gmail.com>
2023-04-24 10:04:14 +0200

    init

---
+++ GraphSwift/GraphSwift/GraphSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphSwiftApp.swift
+//  GraphSwift
+//
+//  Created by Mac on 2023. 04. 24..
+//
+
+import SwiftUI
+
+@main
+struct GraphSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
